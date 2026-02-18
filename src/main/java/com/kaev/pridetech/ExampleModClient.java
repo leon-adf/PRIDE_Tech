86671f5a4d394faa8c385ab0aeb2825d8d20ff44
leon-adf <leonclf13@gmail.com>
2026-02-17 21:53:28 -0300

    Project Setup.

--- src/main/java/com/kaev/pridetech/ExampleModClient.java
+++ src/main/java/com/kaev/pridetech/ExampleModClient.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.kaev.pridetech;
 
 import net.minecraft.client.Minecraft;
 import net.neoforged.api.distmarker.Dist;
@@ -11,11 +11,12 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = ExampleMod.MODID, dist = Dist.CLIENT)
+@Mod(value = PrideTech.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = ExampleMod.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = PrideTech.MOD_ID, value = Dist.CLIENT)
 public class ExampleModClient {
-    public ExampleModClient(ModContainer container) {
+    public ExampleModClient(ModContainer container)
+    {
         // Allows NeoForge to create a config screen for this mod's configs.
         // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
         // Do not forget to add translations for your config options to the en_us.json file.
@@ -23,9 +24,10 @@ public class ExampleModClient {
     }
 
     @SubscribeEvent
-    static void onClientSetup(FMLClientSetupEvent event) {
+    static void onClientSetup(FMLClientSetupEvent event)
+    {
         // Some client setup code
-        ExampleMod.LOGGER.info("HELLO FROM CLIENT SETUP");
-        ExampleMod.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        PrideTech.LOGGER.info("HELLO FROM CLIENT SETUP");
+        PrideTech.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
     }
 }
